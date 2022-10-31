8f2dbc0b7d25f89d0f05531d41c6d22b0e03a95e
Nafis-Fahim50 <nafisf96@gmail.com>
2022-10-31 19:27:40 +0600

    explore about section

---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import person from '../../../assets/images/about_us/person.jpg'
+import parts from '../../../assets/images/about_us/parts.jpg'
+
+const About = () => {
+    return (
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className='relative w-1/2'>
+                    <img src={person} alt='' className="rounded-lg w-96 h-96" />
+                    <img src={parts} alt='' className="absolute right-5 top-28 border-8 border-white w-80 h-80 rounded-lg" />
+                </div>
+                <div className='w-1/2'>
+                    <p className="text-2xl font-bold text-orange-600">About Us</p>
+                    <h1 className="my-5 text-5xl font-bold">
+                        We are qualified <br />
+                        & of experience <br />
+                        in this field</h1>
+                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p className="py-6">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <button className="btn btn-primary">Get More Info</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/Pages/Home/Banner/BannerItem.js
+++ src/Pages/Home/Banner/BannerItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './BannerItem.css'
 
 const BannerItem = ({ slider }) => {
     const {image, id, prev, next} = slider;
     return (
         <div id={`slide${id}`} className="carousel-item relative w-full">
             <div className='carousel-image'>
-                <img src={image} alt='' className="w-full" />
+                <img src={image} alt='' className="w-full rounded-lg" />
             </div>
             <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                 <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
